2148c55c706605961d8b71c27931bdb8cf9f6831
maria <mariafailli42@gmail.com>
2023-08-04 02:00:05 +0200

    Initial Setup
    -setup tailwind & react & themes
    -added base navbar

---
+++ public/lib/theme-util.js
@@ -0,0 +1,30 @@
+
+const getColorPreference = () => {
+    let preference = localStorage.getItem('theme-preference');
+
+    if (!preference) {
+        preference = window.matchMedia('(prefers-color-scheme: dark)').matches
+            ? 'dark'
+            : 'light';
+    }
+
+    return preference;
+};
+
+const setPreference = (themeName) => {
+    localStorage.setItem('theme-preference', themeName);
+    if (themeName === 'light') {
+        document.documentElement.classList.remove('dark');
+    } else {
+        document.documentElement.classList.add('dark');
+    }
+};
+
+function toggleState() {
+    setPreference(getColorPreference() === 'dark' ? 'light' : 'dark');
+}
+(() => {
+    const themeName = getColorPreference();
+
+    setPreference(themeName);
+})();
