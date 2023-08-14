a1d8f7b79f65bad803546528bcb6402d93baeb37
maria <mariafailli42@gmail.com>
2023-08-14 10:49:10 +0200

    update courses

--- src/data/SDUCourses.ts
+++ src/data/SDUCourses.ts
@@ -1,19 +1,23 @@
-const sduCourses: string[] = [
-  'Big Data and Data Science Technologies',
-  'Blockchain Theory and Usage',
-  'Cloud Computing and Edge-Cloud Adaptive Architectures',
-  'Data Security',
-  'Innovative SW Solutions',
-  'Linux for Embedded Objects',
-  'Model-Driven SW Development',
-  'Scientific Methods',
-  'Security in Computer Systems',
-  'SW Engineering of Mobile and Ubiquitous Systems',
-  'SW System Analysis and Verification',
-  'SW Technology for IoT',
-  'SW Architecture',
-  'Statistics and Machine Learning',
-  'Systems Modeling and Simulation'
+interface ISDUCourse {
+  title: string;
+}
+
+const sduCourses: ISDUCourse[] = [
+  { title: 'Big Data and Data Science Technologies' },
+  { title: 'Blockchain Theory and Usage' },
+  { title: 'Cloud Computing and Edge-Cloud Adaptive Architectures' },
+  { title: 'Data Security' },
+  { title: 'Innovative SW Solutions' },
+  { title: 'Linux for Embedded Objects' },
+  { title: 'Model-Driven SW Development' },
+  { title: 'Scientific Methods' },
+  { title: 'Security in Computer Systems' },
+  { title: 'SW Engineering of Mobile and Ubiquitous Systems' },
+  { title: 'SW System Analysis and Verification' },
+  { title: 'SW Technology for IoT' },
+  { title: 'SW Architecture' },
+  { title: 'Statistics and Machine Learning' },
+  { title: 'Systems Modeling and Simulation' },
 ];
 
 export default sduCourses;
