67024aea510be35350832aaec0d1da9b5580f674
maria <mariafailli42@gmail.com>
2023-08-14 10:56:36 +0200

    Update courses structure & imports

---
+++ src/data/sduCourseList.ts
@@ -0,0 +1,23 @@
+interface ISDUCourse {
+  courseName: string;
+}
+
+const sduCourseList: ISDUCourse[] = [
+  { courseName: 'Big Data and Data Science Technologies' },
+  { courseName: 'Blockchain Theory and Usage' },
+  { courseName: 'Cloud Computing and Edge-Cloud Adaptive Architectures' },
+  { courseName: 'Data Security' },
+  { courseName: 'Innovative SW Solutions' },
+  { courseName: 'Linux for Embedded Objects' },
+  { courseName: 'Model-Driven SW Development' },
+  { courseName: 'Scientific Methods' },
+  { courseName: 'Security in Computer Systems' },
+  { courseName: 'SW Engineering of Mobile and Ubiquitous Systems' },
+  { courseName: 'SW System Analysis and Verification' },
+  { courseName: 'SW Technology for IoT' },
+  { courseName: 'SW Architecture' },
+  { courseName: 'Statistics and Machine Learning' },
+  { courseName: 'Systems Modeling and Simulation' },
+];
+
+export default sduCourseList;
---
+++ src/data/viaCourseList.ts
@@ -0,0 +1,31 @@
+export interface IViaCourse {
+  courseName: string;
+}
+
+const viaCourseList: IViaCourse[] = [
+  { courseName: 'Semester Project: Single User System' },
+  { courseName: 'Discrete Mathematics and Algorithms' },
+  { courseName: 'SW Development with UML and Java' },
+  { courseName: 'Web Development' },
+  { courseName: 'Database Systems' },
+  { courseName: 'SW Engineering' },
+  { courseName: 'Semester Project: Client/Server System' },
+  { courseName: 'Database Systems' },
+  { courseName: 'Network and Security' },
+  { courseName: 'Computer Architecture and Organization' },
+  { courseName: 'Semester project: Heterogeneous Systems' },
+  { courseName: '.NET Programming' },
+  { courseName: 'Algorithms and Data Structures' },
+  { courseName: 'DevOps & Cloud' },
+  { courseName: 'Embedded SW' },
+  { courseName: 'Semester Project: Internet of Things' },
+  { courseName: 'Engineering Internship' },
+  { courseName: 'Applied Linear Algebra' },
+  { courseName: 'No-SQL versus relational databases' },
+  { courseName: 'IT Security and Cryptography in Practice' },
+  { courseName: 'Programming Concepts and Languages' },
+  { courseName: 'Digital Multi Media' },
+  { courseName: 'Digital Signal Processing' },
+];
+
+export default viaCourseList;
