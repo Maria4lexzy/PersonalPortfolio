3d67148a5799c30e07c3bae77cae8ca251d866b4
maria <mariafailli42@gmail.com>
2023-09-28 22:18:06 +0200

    remove unused library + udpate MD styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,26 +4,20 @@ import react from "@astrojs/react";
 import netlify from "@astrojs/netlify/functions";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
-import angular from '@analogjs/astro-angular';
-
-
-
-import analogjsangular from "@analogjs/astro-angular";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maria-louisa.com',
-  integrations: [tailwind(), react(), angular({
-    vite: {
-      inlineStylesExtension: 'scss|sass|less',
-      ssr: {
-        // transform these packages during SSR. Globs supported
-        noExternal: ['@rx-angular/**'],
-      },
-    },
-  }), compress(), sitemap({
+  integrations: [tailwind(), react(), compress(), sitemap({
     filter: page => page !== 'https://maria-louisa.com'
-  }), analogjsangular()],
+  }),],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  copyCodeSnippet: {
+    enable: true,
+    buttonText: 'Copy',
+    tooltipText: 'Copy the code snippet',
+    toasterText: 'Copied',
+    toasterDuration: 1500,
+  }
 });
